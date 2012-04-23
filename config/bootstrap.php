bfb9cc601fbbcb5624e1f283a17f54a3d78765aa
Josey Morton <me@joseymorton.com>
2012-04-23 09:44:50 -0500

    adding symfony process class and yui compressor

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,22 +6,32 @@ use lithium\core\Libraries;
 use lithium\storage\Cache;
 use lithium\net\http\Media;
 
+// Plugin Location
 defined('FRONTENDER_PATH') OR define('FRONTENDER_PATH', dirname(__DIR__));
+
+// Vendor Constants
+defined('FRONTENDER_VENDORS') OR define('FRONTENDER_VENDORS', FRONTENDER_PATH . "/vendors");
+	defined('YCOMP_VER') OR define('YCOMP_VER', '2.4.7');
+	defined('YUI_COMPRESSOR') OR define('YUI_COMPRESSOR', FRONTENDER_VENDORS . "/yuicompressor-" . YCOMP_VER . ".jar");
+
+// Library Constants
 defined('FRONTENDER_LIBS') OR define('FRONTENDER_LIBS', FRONTENDER_PATH . "/libraries");
 defined('FRONTENDER_SRC') OR define('FRONTENDER_SRC', FRONTENDER_LIBS . "/assetic/src/Assetic");
 
 /**
  * Pull in asset libraries
  */
+Libraries::add("Symfony", array(
+	"path" => FRONTENDER_LIBS . "/symfony",
+	"bootstrap" => false,
+));
+
 Libraries::add("lessc", array(
-	// "prefix" => "Solarium_",
 	"path" => FRONTENDER_LIBS . "/lessphp",
 	"bootstrap" => "lessc.inc.php",
-	// "loader" => array("Solarium_Autoloader", "load")
 ));
+
 Libraries::add("Assetic", array(
-	// "prefix" => "Solarium_",
 	"path" => FRONTENDER_SRC,
 	"bootstrap" => false,
-	// "loader" => array("Solarium_Autoloader", "load")
 ));
