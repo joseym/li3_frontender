1f43591b548d21fdb22a21a1352a6246d81ef385
Josey Morton <me@joseymorton.com>
2012-04-22 23:34:17 -0500

    Getting Assetic up and running
    Adding less php as a submodule and properly loading assetic library

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,3 +6,22 @@ use lithium\core\Libraries;
 use lithium\storage\Cache;
 use lithium\net\http\Media;
 
+defined('FRONTENDER_PATH') OR define('FRONTENDER_PATH', dirname(__DIR__));
+defined('FRONTENDER_LIBS') OR define('FRONTENDER_LIBS', FRONTENDER_PATH . "/libraries");
+defined('FRONTENDER_SRC') OR define('FRONTENDER_SRC', FRONTENDER_LIBS . "/assetic/src/Assetic");
+
+/**
+ * Pull in asset libraries
+ */
+Libraries::add("lessc", array(
+	// "prefix" => "Solarium_",
+	"path" => FRONTENDER_LIBS . "/lessphp",
+	"bootstrap" => "lessc.inc.php",
+	// "loader" => array("Solarium_Autoloader", "load")
+));
+Libraries::add("Assetic", array(
+	// "prefix" => "Solarium_",
+	"path" => FRONTENDER_SRC,
+	"bootstrap" => false,
+	// "loader" => array("Solarium_Autoloader", "load")
+));
---
+++ config/libraries.php
@@ -0,0 +1,7 @@
+<?php
+
+Libraries::add('Assetic', array(
+	// "prefix" => "assetic_",
+    "path" => FRONTENDER_LIBS . "assetic/src/",
+    "bootstrap" => "functions.php",
+));
